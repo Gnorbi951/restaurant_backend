68b7fe082bebe7e5850a4917eabeb0f667bc9f56
Gőcze Norbert <gnorbi951@gmail.com>
2020-08-17 21:06:38 +0200

    Initial commit

---
+++ src/test/java/com/norbi/gocze/restaurant/RestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norbi.gocze.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
