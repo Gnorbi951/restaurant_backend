991e8232ffffacf1eaec68abf720ad2b89c748c1
Gőcze Norbert <gnorbi951@gmail.com>
2020-08-18 13:11:57 +0200

    Change the domain name

--- src/main/java/com/norbi/gocze/restaurant/controller/MainPage.java
+++ src/main/java/com/norbi/gocze/restaurant/controller/MainPage.java
@@ -1,13 +1,12 @@
 package com.norbi.gocze.restaurant.controller;
 
 import com.norbi.gocze.restaurant.service.ResponseObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "https://dazzling-goldwasser-51c6a3.netlify.app"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://random-takeaway.netlify.app"})
 public class MainPage {
 
 
