18893542aa42878c96410c417c32b8f48fbde0ea
Gőcze Norbert <gnorbi951@gmail.com>
2020-08-18 11:31:24 +0200

    Change crossorigin annotation

--- src/main/java/com/norbi/gocze/restaurant/controller/MainPage.java
+++ src/main/java/com/norbi/gocze/restaurant/controller/MainPage.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "https://dazzling-goldwasser-51c6a3.netlify.app/"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://dazzling-goldwasser-51c6a3.netlify.app/*"})
 public class MainPage {
 
 
