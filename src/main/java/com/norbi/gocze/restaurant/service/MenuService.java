ff1b85fea278141b25faec022d28e03e314d4f2f
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-03 13:32:57 +0200

    Refactor menuservice

--- src/main/java/com/norbi/gocze/restaurant/service/MenuService.java
+++ src/main/java/com/norbi/gocze/restaurant/service/MenuService.java
@@ -2,15 +2,17 @@ package com.norbi.gocze.restaurant.service;
 
 import com.norbi.gocze.restaurant.entity.Menu;
 import com.norbi.gocze.restaurant.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MenuService {
-    @Autowired
-    MenuRepository menuRepository;
+
+    private final MenuRepository menuRepository;
 
     public List<Menu> getAll() {
         return menuRepository.findAll();
