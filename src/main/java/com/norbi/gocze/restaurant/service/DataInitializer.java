ad568a561000ec98bf98277855e0bb30e5409cb1
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-09 17:44:26 +0200

    Add more chicken items

--- src/main/java/com/norbi/gocze/restaurant/service/DataInitializer.java
+++ src/main/java/com/norbi/gocze/restaurant/service/DataInitializer.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 @Service
 @AllArgsConstructor
@@ -59,19 +58,55 @@ public class DataInitializer {
 
 
         Menu friedChicken = Menu.builder().name("Fried Chicken")
-                .picture("https://i.imgur.com/zXZvyga.jpeg")
+                .picture("https://i.imgur.com/HhgHuX3.jpg")
                 .price(1800L)
                 .category(chicken)
                 .build();
+        Menu fontina = Menu.builder().name("Fontina Rolled Chicken")
+                .picture("https://i.imgur.com/FPGxTx1.jpg")
+                .price(2100L)
+                .category(chicken)
+                .build();
+        Menu alfredo = Menu.builder().name("Chicken-Pepper Alfredo")
+                .picture("https://i.imgur.com/S6DwVuc.jpg")
+                .price(1650L)
+                .category(chicken)
+                .build();
+        Menu glazedChicken = Menu.builder().name("Teriyaki Glazed Chicken")
+                .picture("https://i.imgur.com/02DzKsV.jpg")
+                .price(2300L)
+                .category(chicken)
+                .build();
+        Menu bbqChicken = Menu.builder().name("Barbecue Chicken Quesadillas")
+                .picture("https://i.imgur.com/79LA79J.jpg")
+                .price(2140L)
+                .category(chicken)
+                .build();
+        Menu chickenSandwich = Menu.builder().name("Bacon & Swiss Chicken Sandwiches")
+                .picture("https://i.imgur.com/up95lgu.jpg")
+                .price(1900L)
+                .category(chicken)
+                .build();
+        Menu ginger = Menu.builder().name("Ginger-Cashew Chicken Salad")
+                .picture("https://i.imgur.com/IMYp9AA.jpg")
+                .price(2200L)
+                .category(chicken)
+                .build();
+
+
+
 
         pork.setMenu(Arrays.asList(cordonBleu, friedSteak, newOrleansPork, sriracha, dijon,
                                     vietnamese_noodle_salad, grilledPork));
 
-        chicken.setMenu(Collections.singletonList(friedChicken));
+        chicken.setMenu(Arrays.asList(friedChicken, fontina, alfredo, glazedChicken, bbqChicken,
+                                      chickenSandwich, ginger));
 
         categoryRepository.saveAll(Arrays.asList(pork, chicken)); // Important note: save category first
 
         menuRepository.saveAll(Arrays.asList(cordonBleu, friedSteak, friedChicken, newOrleansPork,
-                                            sriracha, dijon, vietnamese_noodle_salad, grilledPork));
+                                            sriracha, dijon, vietnamese_noodle_salad, grilledPork,
+                                            fontina, alfredo, glazedChicken, bbqChicken, chickenSandwich,
+                                            ginger));
     }
 }
