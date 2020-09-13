27a4b7c31189933315853d88f7ab1947012282bd
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-13 19:52:54 +0200

    Trying cross-site config

--- src/main/java/com/norbi/gocze/restaurant/controller/AuthenticationController.java
+++ src/main/java/com/norbi/gocze/restaurant/controller/AuthenticationController.java
@@ -18,7 +18,6 @@ import java.time.Duration;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-@CrossOrigin("https://random-takeaway.netlify.app")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
@@ -45,7 +44,7 @@ public class AuthenticationController {
     private void addTokenToCookie(HttpServletResponse response, String token) {
         ResponseCookie cookie = ResponseCookie.from("token", token)
                 //.domain("random-takeaway.netlify.app") // should be parameterized
-                .sameSite("None")  // CSRF // Changed from "Strict" to "cross-site"
+                .sameSite("cross-site")  // CSRF // Changed from "Strict" to "cross-site"
 //                .secure(true)
                 .maxAge(Duration.ofHours(24))
                 .secure(true)
