39aceda6344955a614829e2a65d48b768dd756df
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-13 18:17:07 +0200

    Change cookie domain to remote

--- src/main/java/com/norbi/gocze/restaurant/controller/AuthenticationController.java
+++ src/main/java/com/norbi/gocze/restaurant/controller/AuthenticationController.java
@@ -46,7 +46,7 @@ public class AuthenticationController {
 
     private void addTokenToCookie(HttpServletResponse response, String token) {
         ResponseCookie cookie = ResponseCookie.from("token", token)
-                .domain("localhost") // should be parameterized
+                .domain("damp-atoll-23585.herokuapp.com") // should be parameterized
                 .sameSite("Strict")  // CSRF
 //                .secure(true)
                 .maxAge(Duration.ofHours(24))
